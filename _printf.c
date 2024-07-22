ce0f2faa640d24f0857f9924f0188a02c1a6dd60
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-22 12:58:57 +0000

    developpement while loop

--- _printf.c
+++ _printf.c
@@ -9,4 +9,30 @@ int _printf(const char *format, ...)
 {
 
 	va_list list;
-	int i;
+	int i = 0;
+	int j = 0;
+
+	convert spec[] = {
+		{"c", _printc},
+		{"s", _prints},
+		{NULL, NULL}
+	};
+	va_start(list, format)
+
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			while (spec[j].check != NULL)
+			{
+				if (*(spec[j].check) == (format[i] + 1))
+				{
+					spec[j].f(list);
+				}
+				j++;
+			}
+			_putchar(format[i]);
+			i++;
+		}
+	}
+}
