a19f6fd34d754f06602fc6af668e990f43f165c5
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-22 12:59:37 +0000

    edited the main.h file and began a rough draft of _printc

---
+++ _printc.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+
+char _printc(char c)
+{
+	if (c < '\0')
+	{
+	_putchar(c);
+	}
+	return (c);
+}
--- main.h
+++ main.h
@@ -7,7 +7,15 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+char _printc(va_list list);
+char _prints(va_list list);
 
+
+/**
+ * struct convert - Structure used in the _printf file for
+ * searching a matching character after a % and execute
+ * a matching function.
+ */
 struct convert
 {
 	char check;
