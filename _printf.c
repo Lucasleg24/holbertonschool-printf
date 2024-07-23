fee93531fb9246089147172b6ea94879dda972a5
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-23 07:46:46 +0000

    Added header, functions and testing file to the main branch

---
+++ _printf.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _printf - recode the function printf
+ * @format: variable with string
+ * Return: return the number of caracter print with the function
+ */
+
+int _printf(const char *format, ...)
+{
+
+	va_list list;
+	int i = 0;
+	int j = 0;
+	int count = 0;
+
+	convert spec[] = {
+		{"c", _printc},
+		{"s", _prints},
+		{NULL, NULL}
+	};
+	va_start(list, format)
+
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			while (spec[j].check != NULL)
+			{
+				if (*(spec[j].check) == (format[i] + 1))
+				{
+					count += spec[j].f(list);
+				}
+				j++;
+			}
+			_putchar(format[i]);
+			count++;
+			i++;
+		}
+	}
+	va_end(list);
+	return (count);
+}
