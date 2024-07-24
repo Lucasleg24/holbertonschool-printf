582730382c6933b142dd8ab587573c3c82b7262e
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-24 13:49:16 +0000

    Edited _printd.c, deleted everything.

--- _printd.c
+++ _printd.c
@@ -9,33 +9,7 @@
 int _printd(va_list list)
 {
 	int count = 0;
-	int i;
-	const char *arg;
-	int d = 1;
-	int result = 1;
-	int c;
 
 	arg = va_arg(list, const char *);
 	
-			for (; *arg != '\0'; arg++)
-			{
-			}
-	i = atoi(arg);
-	result = i;
-	if (result < 0)
-	result = -result;
 
-	while (result / d >= 10)
-	{
-		d *= 10;
-	}
-	while (d > 0)
-	{
-	c = result / d;
-	_putchar('0' + c);
-	result %= d;
-	d /=10;
-	}
-	count++;
-	return (count);
-}
