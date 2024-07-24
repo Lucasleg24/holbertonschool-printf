f53fff4e8819962ee088fd49196d26f1c08b9a85
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-24 09:40:31 +0000

    test NULL error segmentation function

--- _printf.c
+++ _printf.c
@@ -21,14 +21,23 @@ int _printf(const char *format, ...)
 		{NULL, NULL}
 	};
 	va_start(list, format);
-	count += _exec_print();
+	count += _exec_print(list, int i, int j, int count, typedef struct convert_t);
+
+	return (count);
 }
 
 /**
- *
+ * _exec_print - execute the code for return the result at the _printf function
+ * @format: string for print and select the modulo
+ * @list: contain argument
+ * @i: variable for check format
+ * @j: variable for check structure
+ * @count: variable for count the number of caracter print
+ * @convert_t: structure for select the function
+ * Return: return the value of count
  */
 
-int _exec_print(va_list list, int i, int j, int count, struct convert_t)
+int _exec_print(const char *format, va_list list, int i, int j, int count, typedef struct convert_t)
 {
 	while (format != NULL && format[i] != '\0')
 	{
@@ -38,17 +47,19 @@ int _exec_print(va_list list, int i, int j, int count, struct convert_t)
 			{
 				if (*(spec[j].check) == format[i + 1])
 				{
-					count += _exec_print(list);
 					count += spec[j].f(list);
 					i += 2;
-					j = 0;
 				}
 				j++;
 			}
 		}
-		_putchar(format[i]);
-		count++;
-		i++;
+		else
+		{
+			_putchar(format[i]);
+			count++;
+			i++;
+		}
+		j = 0;
 	}
 	va_end(list);
 	return (count);
