bac39a96411f584b92ae8f8c245f81ddfaec386a
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-25 13:34:56 +0000

    Added for git pull reasons

--- _printm.c
+++ _printm.c
@@ -3,12 +3,13 @@
 /**
  * _printm - print modulo caracter
  * @list: argument for print caracter
- * Return: return number of caracter print, 1
+ * Return: return number of caracter print, 1 or 0
  */
 
 int _printm(__attribute__ ((unused)) va_list list)
 {
+	if (list == NULL)
+	return (0);
 	_putchar('%');
-
 	return (1);
 }
